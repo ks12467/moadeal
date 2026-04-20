594c591fec345ca0a99899ec992980d59a4aba90
hyeonlo <ks12467@naver.com>
2026-04-20 16:27:06 +0900

    remove .env file from git repo

---
+++ src/main/java/com/moadeal/moadeal/MoadealApplication.java
@@ -0,0 +1,13 @@
+package com.moadeal.moadeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoadealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoadealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moadeal/moadeal/MoadealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moadeal.moadeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoadealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
